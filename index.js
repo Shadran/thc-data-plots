422b61c3b14a21605ccb286243e03b53434e0e3b
Shadran <crowmaster9281@gmail.com>
2026-04-01 23:10:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 async function getData() {
     const toDownload = {
         "Asska": {
-            "normal": { url: "/data/Asska_res_Normal.txt", name: "Normal" },
-            "adv": { url: "/data/Asska_res_Adv.txt", name: "Advantage" },
-            "thirdAtk": { url: "/data/Asska_res_ThirdAtk.txt", name: "Third Attack" },
-            "thirdAtkAdv": { url: "/data/Asska_res_ThirdAtkAdv.txt", name: "Third Attack Advantage" },
-            "thirdAtkAdvExpandedCrit": { url: "/data/Asska_res_ThirdAtkAdvExpandedCrit.txt", name: "Third Attack Advantage + Expanded Crit" },
+            "normal": { url: "./data/Asska_res_Normal.txt", name: "Normal" },
+            "adv": { url: "./data/Asska_res_Adv.txt", name: "Advantage" },
+            "thirdAtk": { url: "./data/Asska_res_ThirdAtk.txt", name: "Third Attack" },
+            "thirdAtkAdv": { url: "./data/Asska_res_ThirdAtkAdv.txt", name: "Third Attack Advantage" },
+            "thirdAtkAdvExpandedCrit": { url: "./data/Asska_res_ThirdAtkAdvExpandedCrit.txt", name: "Third Attack Advantage + Expanded Crit" },
         },
         "Cowboy": {
-            "normal": { url: "/data/Cowboy_res_Normal.txt", name: "Normal" },
-            "adv": { url: "/data/Cowboy_res_Adv.txt", name: "Advantage" },
-            "thirdAtk": { url: "/data/Cowboy_res_ThirdAtk.txt", name: "Third Attack" },
-            "thirdAtkAdv": { url: "/data/Cowboy_res_ThirdAtkAdv.txt", name: "Third Attack + Advantage" },
+            "normal": { url: "./data/Cowboy_res_Normal.txt", name: "Normal" },
+            "adv": { url: "./data/Cowboy_res_Adv.txt", name: "Advantage" },
+            "thirdAtk": { url: "./data/Cowboy_res_ThirdAtk.txt", name: "Third Attack" },
+            "thirdAtkAdv": { url: "./data/Cowboy_res_ThirdAtkAdv.txt", name: "Third Attack + Advantage" },
         },
         "Morai": {
-            "normal": { url: "/data/Morai_res_Normal.txt", name: "Normal" },
-            "faa": { url: "/data/Morai_res_FirstAtkAdv.txt", name: "First Attack Adv" },
-            "aaa": { url: "/data/Morai_res_AllAtkAdv.txt", name: "All Attacks Adv" },
-            "aaamt": { url: "/data/Morai_res_AllAtkAdvMultiTarget.txt", name: "All Attacks Adv (Multitarget)" },
-            "s": { url: "/data/Morai_res_Smites.txt", name: "Smites" },
-            "sfaa": { url: "/data/Morai_res_SmitesFirstAtkAdv.txt", name: "Smites - First Attack Adv" },
-            "saaa": { url: "/data/Morai_res_SmitesAllAtkAdv.txt", name: "Smites - All Attacks Adv" },
-            "saaamt": { url: "/data/Morai_res_SmitesAllAtkAdvMultiTarget.txt", name: "Smites - All Attacks Adv (Multitarget)" },
+            "normal": { url: "./data/Morai_res_Normal.txt", name: "Normal" },
+            "faa": { url: "./data/Morai_res_FirstAtkAdv.txt", name: "First Attack Adv" },
+            "aaa": { url: "./data/Morai_res_AllAtkAdv.txt", name: "All Attacks Adv" },
+            "aaamt": { url: "./data/Morai_res_AllAtkAdvMultiTarget.txt", name: "All Attacks Adv (Multitarget)" },
+            "s": { url: "./data/Morai_res_Smites.txt", name: "Smites" },
+            "sfaa": { url: "./data/Morai_res_SmitesFirstAtkAdv.txt", name: "Smites - First Attack Adv" },
+            "saaa": { url: "./data/Morai_res_SmitesAllAtkAdv.txt", name: "Smites - All Attacks Adv" },
+            "saaamt": { url: "./data/Morai_res_SmitesAllAtkAdvMultiTarget.txt", name: "Smites - All Attacks Adv (Multitarget)" },
         },
         "Ravane": {
-            "normal": { url: "/data/Ravane_res_Normal.txt", name: "Normal" },
-            "adv": { url: "/data/Ravane_res_Advantage.txt", name: "Advantage" },
-            "hid": { url: "/data/Ravane_res_Hidden.txt", name: "Hidden" },
+            "normal": { url: "./data/Ravane_res_Normal.txt", name: "Normal" },
+            "adv": { url: "./data/Ravane_res_Advantage.txt", name: "Advantage" },
+            "hid": { url: "./data/Ravane_res_Hidden.txt", name: "Hidden" },
         }
     }
 
